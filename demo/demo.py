822167b3635bbd5e9086bd1cf9e2632ca2b84fde
Kyle Adams <kyle.adams@canonical.com>
2014-12-08 11:49:53 -0500

    Cleanup to prepare for pyramid_switchboard release.

--- demo/demo.py
+++ demo/demo.py
@@ -5,6 +5,7 @@ from wsgiref.simple_server import make_server
 
 from switchboard import operator
 
+
 def main_view(request):
     if operator.is_active('test1', request):
         message = 'The "test1" switch is active'
@@ -12,12 +13,23 @@ def main_view(request):
         message = 'The "test1" switch is not active'
     return Response(message)
 
-if __name__ == '__main__':
+
+def main():
     config = Configurator()
     config.add_route('main', '/')
     config.add_view(main_view, route_name='main')
     config.include('pyramid_mako')
     config.include('pyramid_switchboard')
-    app = config.make_wsgi_app()
+    return config.make_wsgi_app()
+
+
+if __name__ == '__main__':
+    app = main()
     server = make_server('0.0.0.0', 8080, app)
+    print('******************************************')
+    print('Starting server at:')
+    print('  http://localhost:8080/')
+    print('Switchboard admin is at:')
+    print('  http://localhost:8080/_switchboard/')
+    print('******************************************')
     server.serve_forever()
